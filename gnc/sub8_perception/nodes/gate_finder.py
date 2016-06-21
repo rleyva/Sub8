487667b501b5850f0b791a545e86d099bf13b45e
Ralph Leyva <ralphfleyva@gmail.com>
2016-06-21 04:18:17 -0500

    PERCEPTION: Add retooled gate-finder

--- gnc/sub8_perception/nodes/gate_finder.py
+++ gnc/sub8_perception/nodes/gate_finder.py
@@ -3,11 +3,10 @@ import sys
 import cv2
 import rospy
 import numpy as np
-
 import image_geometry
 import message_filters
-from std_msgs.msg import Header
-from std_msgs.msg import String
+
+from std_msgs.msg import Header, String
 from geometry_msgs.msg import PointStamped, Point
 
 from cv_bridge import CvBridge, CvBridgeError
@@ -16,40 +15,31 @@ from sensor_msgs.msg import CameraInfo, Image
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-from pykalman import KalmanFilter
-
 class Gate:
     def __init__(self):
         # Throwback to the good ol' days (We'll have to fine tune for the situation)
-        self.yellow_min = np.array([0, 50,50], np.uint8)
-        self.yellow_max = np.array([29,250,250], np.uint8)
+        self.yellow_min = np.array([0, 50, 50], np.uint8)
+        self.yellow_max = np.array([29, 250, 250], np.uint8)
 
         # Orientation information
-        self.point_publisher = rospy.Publisher("gate_points", PointStamped, queue_size = 4)
-
+        self.point_publisher = rospy.Publisher("gate_points", PointStamped, queue_size=4)
         self.gate_pose = None
-        self.gate_distance = None
+        self.approx_gate_distance = None
 
-        '''
-        self.kalman_trackers = []
-
-        for i in range(1,4):
-            KalmanFilter(transition_matrices = [[1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0], [0, 0, 0, 1]],
-                         observation_matrices = [[0.1, 0.5], [-0.3, 0.0]])
-        '''
 
 class GateFinder:
+    # TODO: Make scale invariant!!!!
     def __init__(self, gate):
         self.gate = gate
         self.last_image = None
-        self.last_time_stamp = None
+        self.last_image_time_stamp = None
         self.last_draw_image = None
 
         self.lcam_info, self.rcam_info = None, None
         self.lP, self.rP = None, None
 
-        self.lcam_info_sub = rospy.Subscriber('/stereo/left/camera_info', CameraInfo, self.cinfo_cb, callback_args = 'l')
-        self.rcam_info_sub = rospy.Subscriber('/stereo/right/camera_info', CameraInfo, self.cinfo_cb, callback_args = 'r')
+        self.lcam_info_sub = rospy.Subscriber('/stereo/left/camera_info', CameraInfo, self.cinfo_cb, callback_args='l')
+        self.rcam_info_sub = rospy.Subscriber('/stereo/right/camera_info', CameraInfo, self.cinfo_cb, callback_args='r')
 
         # Hacky shit to get the camera info and unsubscribe
         # TODO: Implement service that gathers camera_info
@@ -57,8 +47,8 @@ class GateFinder:
         while self.lcam_info is None or self.rcam_info is None:
             pass
 
-        print 'Left Camera Info: ', self.lcam_info
-        print 'Right Camera Info: ', self.rcam_info, '\n'
+        rospy.loginfo('Left Camera Info: %s', self.lcam_info)
+        rospy.loginfo('Right Camera Info: %s', self.rcam_info)
 
         self.lcam_info_sub.unregister()
         self.rcam_info_sub.unregister()
@@ -83,24 +73,22 @@ class GateFinder:
         kernel = np.ones((scale, scale)) * -1
         midpoint = (scale // 2, scale // 2)
         cv2.circle(kernel, midpoint, midpoint[0], 1, -1)
-
         mean, std_dev = cv2.meanStdDev(image)
-
         return cv2.filter2D((image - mean) / std_dev, -1, kernel)
 
     def find_contours(self, image, cam):
         # TODO: Resize image to process faster
         # TODO: Limit this to the largest three contours
-        # TODO: Kalman filter centroid locations (Or time-filter the contours themselves)
         # TODO: PCA for orientation (Differentiating b/w long and tall tubes)
         # TODO: Work on discriminating reflections (Should not be a problem at the transdec)
+        # TODO: Convolve gate-type kernel @ different resolutions
 
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         ghsv = cv2.GaussianBlur(hsv,(9,9),0)
         mask = cv2.inRange(ghsv, self.gate.yellow_min, self.gate.yellow_max)
 
         h, s, v = cv2.split(ghsv)
-        v = cv2.bitwise_and(v, v, mask = mask)
+        v = cv2.bitwise_and(v, v, mask=mask)
         v = self.ncc(v)
 
         contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
@@ -122,7 +110,7 @@ class GateFinder:
             box_points = cv2.cv.BoxPoints(rect)
 
             box = [np.int0(cv2.cv.BoxPoints(rect))]
-            cv2.drawContours(image, box, 0, (0,0,255), 2)
+            cv2.drawContours(image, box, 0, (0, 0, 255), 2)
 
             # Let's get some box midpoints
             pts = [list(point) for point in box_points]
@@ -132,8 +120,8 @@ class GateFinder:
                               tuple((np.add(pts[2][:], pts[1][:]) // 2).astype(int))])
 
         for i, point in enumerate(plane_pts):
-            cv2.circle(image, point, 4, (255,0,0), -1)
-            cv2.putText(image, str(i), point, cv2.cv.CV_FONT_HERSHEY_SIMPLEX, 4, (255,255,255), 2, cv2.CV_AA)
+            cv2.circle(image, point, 4, (255, 0, 0), -1)
+            cv2.putText(image, str(i), point, cv2.cv.CV_FONT_HERSHEY_SIMPLEX, 4, (255, 255, 255), 2, cv2.CV_AA)
 
         if len(plane_pts) == 4:
             # If we have enough points for reconstruction
@@ -141,23 +129,22 @@ class GateFinder:
 
             # Camera Shenanigans
             if cam == 'l':
-                self.lP = np.asarray(self.lcam_info.P).reshape((3, 4))
+       i         self.lP = np.asarray(self.lcam_info.P).reshape((3, 4))
                 P = self.lP
+                return plane_pts
             elif cam == 'r':
                 self.rP = np.asarray(self.rcam_info.P).reshape((3, 4))
                 P = self.rP
+                return plane_pts
             else:
-                print 'NAN Argument'
-            return plane_pts
+                rospy.logerr('Invalid camera parameter provided')
+                return None
         else:
             return None
 
     def visualize_pts(self, p_points):
         # Accepts a list of four points in order to generate a plane
         # Returns dims, and angles
-        pass
-
-    def filter_kp(self, points):
         pass
 
     def triangulate_Linear_LS(self, lpoint, rpoint, lP, rP):
@@ -177,9 +164,9 @@ class GateFinder:
         print 'Left Point: ', lpoint
         print 'Right Point: ', rpoint, '\n'
 
-        A = np.zeros((4,3))
-        B = np.zeros((4,1))
-        X = np.zeros((3,1))
+        A = np.zeros((4, 3))
+        B = np.zeros((4, 1))
+        X = np.zeros((3, 1))
 
         A[0][0] = lpoint[0]*lP[2][0] - lP[0][0]
         A[0][1] = lpoint[0]*lP[2][1] - lP[0][1]
@@ -220,24 +207,21 @@ class GateFinder:
             if l_pts is not None and r_pts is not None:
                 assert len(l_pts) == len(r_pts), 'Lists have different dimensions'
 
-                self.fig = plt.figure()
-                ax = self.fig.add_subplot(111, projection='3d')
-
                 for i, pt in enumerate(l_pts):
                     print 'Attempting to triangulate points:'
                     x_sol.append(self.triangulate_Linear_LS(l_pts[i], r_pts[i], self.lP, self.rP))
 
-                for i in enumerate(x_sol):
-                    if i[2][0] < 0:
-                        break    # Invalid points
+                for i, sol in enumerate(x_sol):
+                    if sol[2][0] < 0:
+                        print sol
+                        rospy.logwarn('Invalid Point')
+                        break    # Invalid point
                     else:
-                        for pts in x_sol:
-                            print 'Plotting: ', pts[0], pts[1], pts[2]
-                            ax.scatter(pts[0], pts[1], pts[2], c = 'r', marker='o')
-                            #plt.show()  # Blocks until window is closed
-                            point = PointStamped(header = Header(stamp = rospy.Time.now(),
-                                                 frame_id = '/stereo/left'), point = Point(pts[0],pts[1],pts[2]))
-                            self.point_publisher(point)
+                        print 'Plotting: ', sol[0], sol[1], sol[2]
+                        point = PointStamped(header=Header(stamp=rospy.Time.now(),
+                                                 frame_id='/stereo_front/left'), point=Point(sol[0],sol[1],sol[2]))
+                        self.gate.point_publisher.publish(point)
+                print '\n'
             else:
                 pass
         except CvBridgeError as e:
@@ -252,5 +236,4 @@ if __name__ =='__main__':
         rospy.spin()
     except KeyboardInterrupt:
         print "Shutting Down"
-
     cv2.destroyAllWindows()
